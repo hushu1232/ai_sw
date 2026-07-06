59c94e6fba337429c6a02d95d98f677d8ccd5760
shunwuyu2020 <shunwu2001@163.com>
2026-07-06 16:39:08 +0800

    action

---
+++ ai/agent_in_action/hello-langchain/index.mjs
@@ -0,0 +1,12 @@
+import { chatOpenAI} from '@langchain/openai';
+import 'dotenv/config';
+
+const model = new chatOpenAI({
+  modelName:'deepseek-v4-flash',
+  apiKey: process.env.DEEPSEEK_API_KEY,
+  configuration: {
+    baseURL: 'https://api.deepseek.com/v1',
+  },
+});
+
+const response = await model.invoke('')
